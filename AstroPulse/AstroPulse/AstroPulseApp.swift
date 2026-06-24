b3e0390a39920f87f3ed5150f4346d0f974d4f71
Myawk0 <goditskayan@mail.ru>
2026-06-24 13:46:20 +0300

    feat: init Xcode project

---
+++ AstroPulse/AstroPulse/AstroPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPulseApp.swift
+//  AstroPulse
+//
+//  Created by Мявкo on 24.06.26.
+//
+
+import SwiftUI
+
+@main
+struct AstroPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
